7d46633e387eb312b3fa31b9a92c8394fc9bd867
minsangKwak <kmsdevwork@gmail.com>
2022-03-12 15:06:11 +0900

    first commit

---
+++ js/main/main.js
@@ -0,0 +1,12 @@
+let swiper = new Swiper(".visual-swiper", {
+  spaceBetween: 0,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    // disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
