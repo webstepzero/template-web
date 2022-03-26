6bb909f800aa1acdd5630ddf05f24dfd23834a30
minsangKwak <kmsdevwork@gmail.com>
2022-03-26 14:02:27 +0900

    [feat] header, footer include처리

---
+++ js/common/include.js
@@ -0,0 +1,39 @@
+function includeHTML() {
+  let z, elmnt, file, xhttp;
+
+  z = document.getElementsByTagName("*");
+
+  for (let i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    file = elmnt.getAttribute("data-include");
+
+    if (file) {
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+          // 통신이 완료되면
+          if (this.status == 200) {
+            // 정상적으로 잘 켜짐
+            elmnt.innerHTML = this.responseText;
+          }
+          if (this.status == 404) {
+            // 404에러 - 통신거부
+            elmnt.innerHTML = "Page not found.";
+          }
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("data-include");
+          includeHTML();
+        } //if
+      }; //onreadystatechange
+
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      return;
+    } //if - file
+  } //for
+} //includeHTML
+
+/* ✨ 실행 */
+window.addEventListener("DOMContentLoaded", () => {
+  includeHTML();
+});
